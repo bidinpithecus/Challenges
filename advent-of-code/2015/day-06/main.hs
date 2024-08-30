777b46ca07fcea80ef8a304beb4bad34b418488b
bidinpithecus <vinibidin@gmail.com>
2024-08-30 13:50:35 -0300

    part 2 of 2015/6 done

--- advent-of-code/2015/day-06/main.hs
+++ advent-of-code/2015/day-06/main.hs
@@ -1,40 +1,55 @@
-import System.Environment (getArgs)
+import Data.List qualified as List
+import Data.Map qualified as Map
+import Data.Set qualified as Set
 import Parser
+import System.Environment (getArgs)
 import Text.Parsec
-import Data.List (nub, sort, foldl', union, (\\))
-import qualified Data.Set as Set
-import Data.Set (Set)
 
-type Lights = Set (Int, Int)
+type LightsSet = Set.Set (Int, Int)
+
+type LightsMap = Map.Map (Int, Int) Int
 
 extractCommand :: Either ParseError Command -> Command
 extractCommand command = case command of
   Left err -> error $ "Parsing error: " ++ show err
   Right comm -> comm
 
-countLightsLit :: Lights -> Int
-countLightsLit = Set.size
-
-changeLightsStatusList :: [(Int, Int)] -> Command -> [(Int, Int)]
-changeLightsStatusList lights (TurnOn (x0, y0) (x1, y1)) = lights `union` ([(x, y) | x <- [x0..x1], y <- [y0..y1]])
-changeLightsStatusList lights (TurnOff (x0, y0) (x1, y1)) = lights \\ [(x, y) | x <- [x0..x1], y <- [y0..y1]]
-changeLightsStatusList lights (Toggle (x0, y0) (x1, y1)) =
-    foldl' toggleLight lights [(x, y) | x <- [x0..x1], y <- [y0..y1]]
-    where
-        toggleLight acc (x, y) = if (x, y) `elem` acc then filter (/= (x, y)) acc else (x, y) : acc
-
-changeLightsStatusSet :: Lights -> Command -> Lights
-changeLightsStatusSet lights (TurnOn (x0, y0) (x1, y1)) = Set.union lights (Set.fromList [(x, y) | x <- [x0..x1], y <- [y0..y1]])
-changeLightsStatusSet lights (TurnOff (x0, y0) (x1, y1)) = Set.difference lights (Set.fromList [(x, y) | x <- [x0..x1], y <- [y0..y1]])
+changeLightsStatusSet :: LightsSet -> Command -> LightsSet
+changeLightsStatusSet lights (TurnOn (x0, y0) (x1, y1)) = Set.union lights (Set.fromList [(x, y) | x <- [x0 .. x1], y <- [y0 .. y1]])
+changeLightsStatusSet lights (TurnOff (x0, y0) (x1, y1)) = Set.difference lights (Set.fromList [(x, y) | x <- [x0 .. x1], y <- [y0 .. y1]])
 changeLightsStatusSet lights (Toggle (x0, y0) (x1, y1)) =
-    foldl' toggleLight lights [(x, y) | x <- [x0..x1], y <- [y0..y1]]
-    where
-        toggleLight acc (x, y) = if Set.member (x, y) acc then Set.delete (x, y) acc else Set.insert (x, y) acc
+  List.foldl' toggleLight lights [(x, y) | x <- [x0 .. x1], y <- [y0 .. y1]]
+  where
+    toggleLight acc (x, y) = if Set.member (x, y) acc then Set.delete (x, y) acc else Set.insert (x, y) acc
 
-applyCommandsList lights commands = foldl changeLightsStatusList lights commands
+mapUnionWithCount :: LightsMap -> LightsMap -> LightsMap
+mapUnionWithCount = Map.unionWith (+)
 
-applyCommandsSet :: Lights -> [Command] -> Lights
-applyCommandsSet = foldl' changeLightsStatusSet
+controlBrightness :: LightsMap -> LightsMap
+controlBrightness = Map.map (`max` 0)
+
+countBrightness :: LightsMap -> Int
+countBrightness = Map.foldl (+) 0
+
+changeLightsStatusMap :: LightsMap -> Command -> LightsMap
+changeLightsStatusMap lights (TurnOn (x0, y0) (x1, y1)) =
+  mapUnionWithCount lights newLights
+  where
+    newLights = Map.fromList [((x, y), 1) | x <- [x0 .. x1], y <- [y0 .. y1]]
+changeLightsStatusMap lights (TurnOff (x0, y0) (x1, y1)) =
+  controlBrightness $ mapUnionWithCount lights newLights
+  where
+    newLights = Map.fromList [((x, y), -1) | x <- [x0 .. x1], y <- [y0 .. y1]]
+changeLightsStatusMap lights (Toggle (x0, y0) (x1, y1)) =
+  mapUnionWithCount lights newLights
+  where
+    newLights = Map.fromList [((x, y), 2) | x <- [x0 .. x1], y <- [y0 .. y1]]
+
+applyCommandsSet :: LightsSet -> [Command] -> LightsSet
+applyCommandsSet = List.foldl' changeLightsStatusSet
+
+applyCommandsMap :: LightsMap -> [Command] -> LightsMap
+applyCommandsMap = List.foldl' changeLightsStatusMap
 
 mainSet :: IO ()
 mainSet = do
@@ -45,19 +60,19 @@ mainSet = do
 
   print $ length lightsLit
 
-mainList :: IO ()
-mainList = do
+mainMap :: IO ()
+mainMap = do
   content <- readFile "input.txt"
   let linesList = lines content
   let commands = map (extractCommand . parseCommand) linesList
-  let lightsLit = applyCommandsList [] commands
+  let lightsLit = applyCommandsMap Map.empty commands
 
-  print $ length lightsLit
+  print $ countBrightness lightsLit
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["set"] -> mainSet
-    ["list"] -> mainList
-    _        -> putStrLn "Usage: program_name (set|list)"
+    ["--part-1"] -> mainSet
+    ["--part-2"] -> mainMap
+    _ -> putStrLn "Usage: program_name (--part-1|--part-2)"
